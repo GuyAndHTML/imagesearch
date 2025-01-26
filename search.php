531ecffa3596ac4869800022ca45bcc45ea3cebd
orange man <83247090+GuyAndHTML@users.noreply.github.com>
2025-01-26 01:14:54 +0100

    setup for xampp

--- search.php
+++ search.php
@@ -2,7 +2,7 @@
 // Folder where images are stored
 $imageFolder = 'images';
 
-// Get the search query
+// Get the search query from the URL
 $query = isset($_GET['q']) ? $_GET['q'] : '';
 
 // Initialize an array to hold the image filenames
@@ -15,6 +15,7 @@ if (is_dir($imageFolder)) {
 
     // Loop through the files and check for matches
     foreach ($files as $file) {
+        // If the file is an image and contains the search query (case-insensitive)
         if (stripos($file, $query) !== false && preg_match('/\.(jpg|jpeg|png|gif)$/i', $file)) {
             $matchingImages[] = $file;
         }
